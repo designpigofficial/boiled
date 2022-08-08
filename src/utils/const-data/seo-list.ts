d4701153fd498efb7c1a6dadfdfee86e8a9935b7
designpigofficial <89279319+designpigofficial@users.noreply.github.com>
2022-08-08 14:32:51 -0600

    Initial commit

---
+++ src/utils/const-data/seo-list.ts
@@ -0,0 +1,16 @@
+// types
+import { SeoProps } from "types/components/Seo";
+
+// ------------------------------------------------------------------
+
+export const informations: { [any: string]: SeoProps } = {
+  home: {
+    title: "Next-Boilerplate By ❤ GOLD LION❤",
+    description: "🖐Welcome to visit for 😊my boilerplate😊.",
+    keywords: "Next React Redux GraphQL styled-component Boilerplate GOLD LION",
+  },
+  404: {
+    title: "☹ OOPS!!! ☹ Page not be found",
+    description: "We can't find your url in our project.🙄",
+  },
+};
